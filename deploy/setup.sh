83a36666a10ae60e59e4f5448cac93718868205a
Elisha Avior <64099551+eavior@users.noreply.github.com>
2021-10-17 16:30:45 +0300

    deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,11 +5,11 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/eavior/django-server.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/django-server'
 
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
